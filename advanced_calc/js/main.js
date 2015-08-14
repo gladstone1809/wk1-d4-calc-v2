3a7d6be5f43e6a88d90cb1f73c1c17d47284e365
Chris <chrisjcw@gmail.com>
2015-08-14 07:09:38 +0100

    Basic & part of BMI

--- advanced_calc/js/main.js
+++ advanced_calc/js/main.js
@@ -29,3 +29,34 @@ calculator.addEventListener('click', function () {
     document.getElementById("basic-answer").className = "show";
     document.getElementById("basic-answer-alert").innerHTML = ans1;
 });
+
+
+var calculator = document.getElementById("bmi-calc");
+
+calculator.addEventListener('click', function () {
+  var mass = parseFloat(document.getElementById("bmi-mass").value);
+  var height = parseFloat(document.getElementById("bmi-height").value);
+  var units = document.getElementById("bmi-units").value;
+
+  console.log(mass);
+  console.log(height);
+  console.log(units)
+  
+  debugger;
+if (units === "imperial") {
+  var ans2 = Math.exp(mass, 2) / height * 703;
+  document.getElementById("bmi-mass-unit").innerHTML = "lb"; 
+  document.getElementById("bmi-height-unit").innerHTML = "in";
+
+} else if (units === "metric") {
+  var ans2 = Math.exp(mass, 2) / height;
+  document.getElementById("bmi-mass-unit").innerHTML = "kg"; 
+  document.getElementById("bmi-height-unit").innerHTML = "m";
+};
+
+  console.log(ans2);
+
+  document.getElementById("bmi-answer").className = "show";
+  document.getElementById("bmi-answer-alert").innerHTML = ans2;
+});
+
